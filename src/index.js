df4e7185201c88813248d2c6e4abfa162d402e72
helinjiang <me@helinjiang.com>
2018-04-30 22:02:10 +0800

    feat: 新增 getUrl 方法

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import { query, param } from './util';
 
+/**
+ * 合并 URL 和 param 参数为一个新的URL
+ *
+ * @param {String} url 请求的URL
+ * @param {Object} obj 需要序列化的对象
+ * @param {Boolean} [keepAll] 保留所有的参数，默认为false，此时只有key而没有value的情况就会被忽略
+ * @return {String}
+ * @author linjianghe
+ */
+function getUrl(url, obj, keepAll) {
+    if (obj) {
+        let paramStr = param(obj, keepAll);
+        if (paramStr) {
+            url = url + (url.match(/\?/) ? '&' : '?') + paramStr;
+        }
+    }
+
+    return url;
+}
+
 module.exports = {
     query: query,
-    param: param
+    param: param,
+    getUrl: getUrl
 };
